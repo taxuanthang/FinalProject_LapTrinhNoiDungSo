f8229aa89af29dc81d2e042dfb67deabf62e76dc
dvcchamhocvcl <dvcchamhoc@gmail.com>
2024-11-25 22:35:29 +0700

    hehe

--- Assets/Thang.cs
+++ Assets/Thang.cs
@@ -7,7 +7,7 @@ public class Thang : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+	//Taxuan thang        
     }
 
     // Update is called once per frame
