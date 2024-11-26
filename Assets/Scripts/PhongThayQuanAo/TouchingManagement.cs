0e7c843daa390a54fe38d01bbf9a4d0e46887174
taxuanthang <thangtaxuan65@gmail.com>
2024-11-26 15:28:28 +0700

    complete TuongTac trong phong thay quan ao

---
+++ Assets/Scripts/PhongThayQuanAo/TouchingManagement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class TouchingManagement : MonoBehaviour
+{
+
+    [SerializeField] GameObject AccessoriesTrigger;
+    [SerializeField] GameObject SkinTrigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ClickedObject = getClickedObject(out RaycastHit hit);
+            if (AccessoriesTrigger == ClickedObject)
+            {
+                print("Clicked on Acessories");
+            }
+            if (SkinTrigger == ClickedObject)
+            {
+                print("Clicked on Skin");
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //print("Remove");
+        }
+    }
+
+
+    GameObject getClickedObject(out RaycastHit hit)
+    {
+        GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+        {
+            if (!isPointerOverUIObject())
+            {
+                target = hit.collider.gameObject;
+            }
+        }
+        return target;
+    }
+    private bool isPointerOverUIObject()
+    {
+        PointerEventData ped = new PointerEventData(EventSystem.current);
+        ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        //print(ped);
+        List<RaycastResult> results = new List<RaycastResult>();
+        //print(results);
+        EventSystem.current.RaycastAll(ped, results);
+        //print(results);
+        return results.Count > 0;
+    }
+}
